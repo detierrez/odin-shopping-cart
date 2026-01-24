2e86d772f3c7a24cdf49b0a6235596c229462884
detierrez <detierrez@gmail.com>
2026-01-24 00:45:57 -0300

    Implement ShoppingBag component

--- src/components/Layout/Navbar/Navbar.jsx
+++ src/components/Layout/Navbar/Navbar.jsx
@@ -1,12 +1,18 @@
 import { Link } from "react-router";
 import styles from "./Navbar.module.css";
+import ShoppingBag from "../../ShoppingBag/ShoppingBag";
 
 function Navbar({ isTransparent, totalItems }) {
   const className = `${styles.navbar} ${isTransparent && styles.transparent}`;
   return (
     <nav className={className}>
       <Link to="/">LOGO</Link>
-      {!isTransparent && <Link to="cart">Cart ({totalItems})</Link>}
+
+      {!isTransparent && (
+        <Link to="cart">
+          <ShoppingBag count={totalItems} />
+        </Link>
+      )}
     </nav>
   );
 }
